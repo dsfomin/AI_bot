0122f43a71bb7bdcc9434518c081b54083dcef07
dsfomin <dsfomin2@gmail.com>
2020-06-11 11:50:28 +0300

    ADD bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import apiai, json
+updater = Updater(token='YOUR TOKEN') # Token API Telegram
+dispatcher = updater.dispatcher
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+def textMessage(bot, update):
+    request = apiai.ApiAI('YOUR TOKEN').text_request() # Token Dialogflow
+    request.lang = 'ru'
+    request.session_id = 'BatlabAIBot'
+    request.query = update.message.text
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech']
+    if response:
+        bot.send_message(chat_id=update.message.chat_id, text=response)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+updater.start_polling(clean=True)
+updater.idle()
